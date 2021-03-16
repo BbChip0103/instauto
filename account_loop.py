8d00614f4c8c045ede0ab0f86bbda95a7446356e
bbchip0103 <bbchip13@gmail.com>
2021-03-17 04:05:46 +0900

    Add new function, user select, exit

--- account_loop.py
+++ account_loop.py
@@ -77,18 +77,7 @@ def login_loop(browser, username, passwd):
     if i >= 29:
         assert False
 
-    ### is working done loop
-    while True:
-        pressed_key = input('Next user: press \'n\', Previous user: press \'p\': ')
-        pressed_key = pressed_key.lower()
-        if pressed_key == 'n':
-            break
-        elif pressed_key == 'p':
-            break
-        else:
-            print('{} is unknown keyword'.format(pressed_key))
-
-    return pressed_key
+    return True
 
 
 if __name__ == '__main__':
@@ -101,13 +90,55 @@ if __name__ == '__main__':
     proxy = None
 
     id_i = 0
+    username = id_list[id_i][0]
+
+    ### is working done loop
+    is_end = False
     while True:
+        pressed_key = input('Next: \'n\', Previous: \'p\', Select user: \'u\', End: \'e\': ')
+        pressed_key = pressed_key.lower()
+        if pressed_key == 'n':
+            if (len(id_list)-1) == id_i:
+                print('{} is last user'.format(username))
+            else:
+                break
+        elif pressed_key == 'p':
+            if id_i == 0:
+                print('{} is first user'.format(username))
+            else:
+                break            
+        elif pressed_key == 'e':
+            is_end = True
+            break
+        elif pressed_key == 'u':
+            print('=== Username list ===')
+            for id_j, user_inform in enumerate(id_list):
+                each_username = user_inform[0]
+                print('{}. {}'.format(id_j, each_username))
+            print('=====================')
+            pressed_key_2 = input('Select user number(\'b\' is back): ')
+            pressed_key_2 = pressed_key_2.lower()
+
+            if pressed_key_2 == 'b':
+                pass
+            elif pressed_key_2.isdigit():
+                user_number = int(pressed_key_2)
+                if user_number >= len(id_list):
+                    print('{} is unknown usernumber'.format(user_number))
+                else:
+                    id_i = user_number
+                    break
+        else:
+            print('{} is unknown keyword'.format(pressed_key))
+        print()
+
+    while is_end == False:
         if id_i >= len(id_list):
             break
         try:
             username = id_list[id_i][0]
             passwd = id_list[id_i][1]
-            
+
             if proxy is not None:
                 webdriver.DesiredCapabilities.CHROME['proxy'] = {
                     "httpProxy": proxy,
@@ -133,20 +164,52 @@ if __name__ == '__main__':
             options.add_argument('--user-agent="Mozilla/5.0 (Linux; Android 9; SM-A102U Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Instagram 155.0.0.37.107 Android (28/9; 320dpi; 720x1468; samsung; SM-A102U; a10e; exynos7885; en_US; 239490550)"')
 
             browser = webdriver.Chrome("chromedriver_win32/chromedriver.exe", options=options)
-            
+
             print('{}/{}, Current user: {}'.format(id_i+1, len(id_list), username))
-            pressed_key = login_loop(browser, username, passwd)
+            is_worked_well = login_loop(browser, username, passwd)
+
+            ### is working done loop
+            is_end = False
+            while True:
+                pressed_key = input('Next: \'n\', Previous: \'p\', Select user: \'u\', End: \'e\': ')
+                pressed_key = pressed_key.lower()
+                if pressed_key == 'n':
+                    if (len(id_list)-1) == id_i:
+                        print('{} is last user'.format(username))
+                    else:
+                        break
+                elif pressed_key == 'p':
+                    if id_i == 0:
+                        print('{} is first user'.format(username))
+                    else:
+                        id_i -= 2
+                        break  
+                elif pressed_key == 'e':
+                    is_end = True
+                    break
+                elif pressed_key == 'u':
+                    print('=== Username list ===')
+                    for id_j, user_inform in enumerate(id_list):
+                        each_username = user_inform[0]
+                        print('{}. {}'.format(id_j, each_username))
+                    print('=====================')
+                    pressed_key_2 = input('Select user number(\'b\' is back): ')
+                    pressed_key_2 = pressed_key_2.lower()
+                    
+                    if pressed_key_2 == 'b':
+                        pass
+                    elif pressed_key_2.isdigit():
+                        user_number = int(pressed_key_2)
+                        if user_number >= len(id_list):
+                            print('{} is unknown usernumber'.format(user_number))
+                        else:
+                            id_i = user_number-1
+                            break
+                else:
+                    print('{} is unknown keyword'.format(pressed_key))
+                print()
             browser.quit()
 
-            if pressed_key == 'p': 
-                id_i -= 2 
-                if id_i == -2:
-                    print('{} is first user'.format(username))
-                    id_i = -1 
-            elif pressed_key == 'n':
-                if (len(id_list)-1) == id_i:
-                    print('{} is last user'.format(username))
-            
             id_i += 1
         except Exception as e:
             id_i += 1
@@ -157,3 +220,5 @@ if __name__ == '__main__':
             # browser.quit()
             print()
             time.sleep(1)
+            
+    print('Loop end!')
